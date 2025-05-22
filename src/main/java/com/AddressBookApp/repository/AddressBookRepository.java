d8cedf63f96e4294a462f14e03f5f9cfd63879f6
ananyagaur-23 <ananyagaur.23@gmail.com>
2025-05-22 10:29:58 +0530

    Initialize Spring Boot AddressBook project with REST support

--- src/main/java/com/AddressBookApp/repository/AddressBookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.AddressBookApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.AddressBookApp.model.AddressBookData;
-
-public interface AddressBookRepository extends JpaRepository<AddressBookData, Integer> {
-}
